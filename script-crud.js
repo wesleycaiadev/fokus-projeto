25dafab704642780d30bce748f546930a63eb99a
wesleycaiadev <wesleycaia.dev@gmail.com>
2026-02-02 21:42:46 -0300

    botão de cancelar

--- script-crud.js
+++ script-crud.js
@@ -2,6 +2,7 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -73,3 +74,10 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
 });
+
+const limparFormulario =() =>{
+    textarea.value =""
+   formAdicionarTarefa.classList.add("hidden")
+}
+
+btnCancelar.addEventListener("click",limparFormulario);
